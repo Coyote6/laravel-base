4a60f65bf00dde5c082a5a9002d26bf0de5bbe01
Tanner Naeher <tan@Tanners-MacBook-Pro.local>
2022-10-27 15:44:33 -0500

    Updated the id field in HasMachineNameAsId trait to use the getKeyName() method to be more versitile.

--- src/Traits/HasMachineNameAsId.php
+++ src/Traits/HasMachineNameAsId.php
@@ -11,8 +11,8 @@ trait HasMachineNameAsId {
 
 		
 	public function createMachineName () {
-		if (is_null ($this->id) || $this->id == '') {
-			$this->id = trim (strtolower (preg_replace ('/[^\da-z]/i','_', $this->name)));
+		if (is_null ($this->{$this->getKeyName()}) || $this->{$this->getKeyName()} == '') {
+			$this->{$this->getKeyName()} = trim (strtolower (preg_replace ('/[^\da-z]/i','_', $this->name)));
 		}
 	}
   
