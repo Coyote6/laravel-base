704ea3226276b66e6268ed23b512aa485e968def
Tanner Naeher <tan@Tanners-MacBook-Pro.local>
2022-10-27 09:56:12 -0500

    Added ReadsCsv trait

---
+++ src/Traits/ReadsCsv.php
@@ -0,0 +1,43 @@
+<?php
+	
+	
+namespace App\Traits;
+
+
+trait ReadsCsv {
+	
+	
+	//
+	// @return array
+	//
+	protected function getItemsFromCSV (string $pathToFile = null) {
+		
+		$fh = fopen ($pathToFile, 'r');
+		$items = [];
+		$headers = [];
+		$count = 0;
+		if ($fh !== false) {
+			while (($row = fgetcsv($fh, 1000, ',')) !== false) {
+				if ($count == 0) {
+					foreach ($row as $key => $column) {
+						$headers[$key] = $column;
+					}
+				}
+				else {
+					foreach ($row as $key => $column) {
+						if (!isset ($items[$count])) {
+							$items[$count] = [];
+						}
+						$items[$count][$headers[$key]] = $column;
+					}
+				}
+				$count++;
+			}
+			fclose ($fh);
+		}
+		return $items;
+		
+	}
+	
+
+}
