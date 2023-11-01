ce9218e2b481ade214dd6ad09bb0573f877d377b
Tanner Naeher <tan@Tanners-MacBook-Pro.local>
2023-11-01 09:27:28 -0500

    Added helper functions and autoloaded them.

---
+++ src/Helpers/Helpers.php
@@ -0,0 +1,54 @@
+<?php
+	
+
+//
+// @return string || null
+//
+function getCurrentUserClientId () {
+	$user = auth()->user();
+	if ($user) {
+		return $user->client_id;
+	}
+	return null;
+}
+
+//
+// Change string to snake case.
+//
+// @return void
+//
+function toSnakeCase (string $str) {
+	
+	// Replace spaces and special characters with underscores
+	$snakeCase = preg_replace('/[^A-Za-z0-9]+/', '_', $str);
+	
+	// Convert to lowercase
+	$snakeCase = strtolower($snakeCase);
+	
+	// Remove leading and trailing underscores
+	$snakeCase = trim($snakeCase, '_');
+	return $snakeCase;
+	
+}
+
+//
+// @alias to toSnakeCase()
+//
+function createMachineName (string $str) {
+	return toSnakeCase ($str);
+}
+
+//
+// Change camelcase keys to snake case.
+//
+// @return void
+//
+function snakeCaseKeys (array &$array) {
+	foreach ($array as $key => $val) {
+		$newKey = strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $key));
+		if ($newKey != $key && !isset ($array[$newKey])) {
+			$array[$newKey] = $val;
+			unset ($array[$key]);
+		}
+	}
+}
