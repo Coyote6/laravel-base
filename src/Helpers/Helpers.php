810e01ccced24b46111499c11aa91bb5baecd245
Tanner Naeher <tan@Tanners-MacBook-Pro.local>
2023-11-01 09:29:58 -0500

    Made sure functions do not already exist before defining the functions.

--- src/Helpers/Helpers.php
+++ src/Helpers/Helpers.php
@@ -4,12 +4,14 @@
 //
 // @return string || null
 //
-function getCurrentUserClientId () {
-	$user = auth()->user();
-	if ($user) {
-		return $user->client_id;
+if (!function_exists('getCurrentUserClientId')) {
+	function getCurrentUserClientId () {
+		$user = auth()->user();
+		if ($user) {
+			return $user->client_id;
+		}
+		return null;
 	}
-	return null;
 }
 
 //
@@ -17,38 +19,46 @@ function getCurrentUserClientId () {
 //
 // @return void
 //
-function toSnakeCase (string $str) {
-	
-	// Replace spaces and special characters with underscores
-	$snakeCase = preg_replace('/[^A-Za-z0-9]+/', '_', $str);
-	
-	// Convert to lowercase
-	$snakeCase = strtolower($snakeCase);
-	
-	// Remove leading and trailing underscores
-	$snakeCase = trim($snakeCase, '_');
-	return $snakeCase;
-	
+if (!function_exists('toSnakeCase')) {
+	function toSnakeCase (string $str) {
+		
+		// Replace spaces and special characters with underscores
+		$snakeCase = preg_replace('/[^A-Za-z0-9]+/', '_', $str);
+		
+		// Convert to lowercase
+		$snakeCase = strtolower($snakeCase);
+		
+		// Remove leading and trailing underscores
+		$snakeCase = trim($snakeCase, '_');
+		return $snakeCase;
+		
+	}
 }
+
 
 //
 // @alias to toSnakeCase()
 //
-function createMachineName (string $str) {
-	return toSnakeCase ($str);
+if (!function_exists('createMachineName')) {
+	function createMachineName (string $str) {
+		return toSnakeCase ($str);
+	}
 }
+
 
 //
 // Change camelcase keys to snake case.
 //
 // @return void
 //
-function snakeCaseKeys (array &$array) {
-	foreach ($array as $key => $val) {
-		$newKey = strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $key));
-		if ($newKey != $key && !isset ($array[$newKey])) {
-			$array[$newKey] = $val;
-			unset ($array[$key]);
+if (!function_exists('snakeCaseKeys')) {
+	function snakeCaseKeys (array &$array) {
+		foreach ($array as $key => $val) {
+			$newKey = strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $key));
+			if ($newKey != $key && !isset ($array[$newKey])) {
+				$array[$newKey] = $val;
+				unset ($array[$key]);
+			}
 		}
 	}
 }
